9895b1e76a4e74e6a8ca612d719beeeffeec922b
Tristan Naess <ckytp96@gmail.com>
2023-02-02 20:21:45 -0800

    made string args const ref, added Document and Data class declarations

--- include/database.hpp
+++ include/database.hpp
@@ -5,41 +5,33 @@
 #include "document.hpp"
 
 #include <unordered_map>
+#include <vector>
 #include <initializer_list>
 #include <string>
 
-// -----------------
-//  Modes
-// -----------------
-// Structured memory represents a document as a map in memory
-// Parsed memory stores a document as a (utf-8) string and parses to find fields as requested
-// Temporary stores a number of documents in memory fully parsed, and as necessary cycles recently referenced documents into a temp file for storage
 
-
-// database can be constructed empty with constructor 1 or populated with collections by name, either as a vector or initializer list
+// database can be constructed empty with constructor 1 or populated with collections by name, either as a vector (2) or initializer list (3)
 // added collections populate the documents, with repeated  IDs cross-referenced rather than re-created
-
-enum class Memory {Structured, Parsed, Temporary};
 
 class Database
 {
     public:
-        Database(Memory mode); // ----------------------------------------------- 1
-        Database(Memory mode, std::vector<std::string> collections); // --------- 2
-        Database(Memory mode, std::initializer_list<std::string> collections); // 3
+        Database(Memory mode); // ------------------------------------------------- 1
+        Database(Memory mode, const std::vector<std::string>& collections); // ---- 2
+        Database(Memory mode, std::initializer_list<std::string> collections); // - 3
 
         // All interface is done through the Database class
         // Create
-        insert(std::string document_data, std::string collection);
-        insert(std::string document_data, std::vector<std::string> collections);
-        insert(std::string document_data, std::initializer_list<std::string> collections);
+        insert(const std::string& document_data, const std::string& collection); // add to single collection
+        insert(const std::string& document_data, const std::vector<std::string>& collections); // add to multiple collections
+        insert(const std::string& document_data, std::initializer_list<std::string> collections); // add to multiple collections
 
         // Read/Update
         // decide whether documents can be directly accessed or only by field
 
         // Delete
         remove(size_t id);
-        remove(std::vector<size_t> ids);
+        remove(const std::vector<size_t>& ids);
         remove(std::initializer_list<size_t> ids);
 
     private:
--- include/document.hpp
+++ include/document.hpp
@@ -1,12 +1,70 @@
 #ifndef __DOCUMENT_HPP__
 #define __DOCUMENT_HPP__
 
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <variant>
+
+// -----------------
+//  Modes
+// -----------------
+// Structured memory represents a document as a map in memory
+// Parsed memory stores a document as a (utf-8) string and parses to find fields as requested
+// Temporary stores a number of documents in memory fully parsed, and as necessary cycles recently referenced documents into a temp file for storage
+
+enum class DataType { Object, Array, String, Number, Boolean, Null };
+
+class Data
+{
+    public:
+        Data(const std::string& data);
+
+        // probably replace with a templated function later
+        const std::string& get_string() const;
+        std::string& get_string();
+        const double& get_number() const;
+        double& get_number();
+        // see if number can be split into long and double to better store depending on type
+        const bool& get_boolean() const;
+        bool& get_boolean();
+
+        const Data& get(size_t index) const;
+        Data& get(size_t index);
+        const Data& get(const std::string& key) const;
+        Data& get(const std::string& key);
+
+    private:
+        DataType type;
+        std::variant<
+            std::unordered_map<std::string, Data>,
+            std::vector<Data>, // possibly replace with unordered_map, if ref invalidation is a concern
+            std::string,
+            double,
+            bool> // also used for Null
+        m_data;
+};
+
+/*
+// return wrapper class to allow return of Object and Array data without allowing assignment
+class DataReturnWrapper
+{
+    public:
+        DataReturnWrapper()
+
+    private:
+};
+*/
+
+enum class Memory {Structured, Parsed, Temporary};
+
 class Document
 {
     public:
+        Document(Memory mode, std::string data);
 
     private:
-
+        std::variant<std::string, std::unordered_map<std::string, Data>> m_data;
 };
 
 #endif//__DOCUMENT_HPP__
